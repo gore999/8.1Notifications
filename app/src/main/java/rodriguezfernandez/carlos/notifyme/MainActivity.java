0dedb33a3d4ee40ec2aadd7355dec1150be37b41
Carlos <carloscoucho@gmail.com>
2019-04-07 01:58:52 +0200

    Task 2: Crear métodos para actualizar y cancelar la notificación. Al actualizarla, cambiamos el estilo y añadimos una imagen a la notificación, además de cambiar el texto.

--- app/src/main/java/rodriguezfernandez/carlos/notifyme/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/notifyme/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -15,14 +17,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
-private NotificationManager mNotifyManager;
-private static final int NOTIFICATION_ID=0;
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID=0;
 
     private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //crear botones y añadir listeners.
         button_notify=findViewById(R.id.notify);
         button_notify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,11 +35,31 @@ private static final int NOTIFICATION_ID=0;
                 sendNotification();
             }
         });
+
+        button_update=findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Llamar a updateNotification
+                updateNotification();
+            }
+        });
+        button_cancel=findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelNotification();
+            }
+        });
+        setNotificationButtonState(true, false, false);
+
+        //Crear el canal de notificaciones. Sin esto, no funciona.
         createNotificationChannel();
     }
     public void sendNotification(){
         NotificationCompat.Builder notifyBuilder=getNotificationBuilder();
         mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+        setNotificationButtonState(false, true, true);
     }
     public void createNotificationChannel(){
         mNotifyManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
@@ -60,5 +85,26 @@ private static final int NOTIFICATION_ID=0;
                 .setContentIntent(notificationPendingIntent)//Añadir el pendingintent
                 .setAutoCancel(true);
         return notifyBuilder;
+    }
+    ///METODOS DE TASK 2:
+    //Update notification
+    public void updateNotification(){
+        Bitmap androidImage=BitmapFactory.decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder=getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(androidImage).setBigContentTitle("Notificacion actualizada!"));
+        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+        setNotificationButtonState(false, false, true);
+
+    }
+    public void cancelNotification(){
+        mNotifyManager.cancel(NOTIFICATION_ID);
+        setNotificationButtonState(true, false, false);
+
+    }
+    //Pone los botones en el estado que se indique en los parametros.
+    void setNotificationButtonState(Boolean isNotifyEnabled,Boolean isUpdateEnabled,Boolean isCancelEnabled) {
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
     }
 }
